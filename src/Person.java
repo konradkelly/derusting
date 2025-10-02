320e1d291273f68910409bdc7a38acf71a55cbde
konradkelly <konradky@gmail.com>
2025-10-01 21:06:40 -0700

    added Person.java

--- src/Person.java
+++ src/Person.java
@@ -6,14 +6,22 @@
 public class Person {
   // Declare a public String instance variable for the name of the person
   // Declare a private int instance variable for the age of the person
-
+    public String name;
+    private int age;
 
   // Create a constructor that takes the name and age of the person
   // and assigns it to the instance variables
+    public Person(String name, int age) {
+      this.name = name;
+      this.age = age;
+    }
 
 
   // Create a toString method that gives the name and age of the person
-
+    @Override
+    public String toString() {
+      return "Name: " + name + ", Age: " + age; 
+    }
 
   // Implement the below public instance method "birthYear"
   // There should NOT be any print statement in this method.
@@ -29,24 +37,37 @@ public class Person {
    */
   // (create the instance method here)
 
+  public int birthYear(int currentYear) {
+    return currentYear - age;
+  }
+
 
   public static void main(String[] args) {
     // Create an instance of Person
+    Person Bob = new Person("Bob", 61);
 
     // Create another instance of Person with a different name and age and
     // assign it to a different variable
+    Person Tommy = new Person("Tommy", 5);
 
     // Print the first person
+    System.out.println(Bob);
 
     // Print the second person
+    System.out.println(Tommy);
 
     // Get the name of the first person and store it in a local variable
-
+    String name = Bob.name;
+    System.out.println(name);
+    
     // Using the birthYear method, get the birth year of the first person
     // and store it in a local variable. Input the actual current year (e.g. 2025)
     // as the argument.
+      int AgeOfBob = Bob.birthYear(2025);
+
 
     // In a separate statement, print the local variable holding the birth year.
+      System.out.println(AgeOfBob);
 
     /**
      * Terminology!
